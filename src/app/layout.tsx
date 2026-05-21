63a36423a90282153b0c89bdb07707b4811215e2
julhian <Main@MacBook-Pro-de-charlie.local>
2026-05-21 14:01:54 -0500

    Optimización: Integración de Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "HAWKIN | El Futuro de la IA",
@@ -9,7 +10,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <body className="antialiased bg-black text-white">{children}</body>
+      <body className="antialiased bg-black text-white">
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
